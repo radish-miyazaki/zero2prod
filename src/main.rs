f2928b8c7009651d6bd4dad457052fb78e797dc2
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-03-21 17:30:00 +0900

    impl to output structured logs for obviously

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,28 @@ use api::configuration::get_configuration;
 use api::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // actix-webのlogを含めた、全てのイベントをsubscriberにリダイレクトする
+    LogTracer::init().expect("Failed to set logger");
+
+    // 環境変数RUST_LOGが設定されていない場合、infoレベル以上のログを出力
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    // 構造化されたログを出力するためにtracingを用いている
+    // また、フォーマットするために、tracing_subscriber/tracing_bunyan_formatterも併用
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing_futures::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -11,6 +12,18 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // リクエストを判別するためのユニークなリクエストID
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -22,11 +35,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
