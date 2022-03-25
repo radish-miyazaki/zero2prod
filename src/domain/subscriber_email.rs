76cf505c54276c7aa842598a21dc3362ab977c15
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-03-25 14:51:45 +0900

    add email valid test

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,7 +22,9 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claim::assert_err;
+    use claim::{assert_err, assert_ok};
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn empty_string_is_reject() {
@@ -40,5 +42,11 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@gmail.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        assert_ok!(SubscriberEmail::parse(email));
     }
 }
