6b14724cc817c2ecbd08f0e4cfb169385cf4ee00
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-03-26 22:53:33 +0900

    add settings when running application

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // INFO: Skip: 5.4 Deploy To DigitalOcean Apps Platform
 
-// NEXT: 6.10 Handling A Result
+// NEXT: 7.2.5.0.2 Body
 use api::configuration::get_configuration;
+use api::email_client::EmailClient;
 use api::startup::run;
 use api::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
@@ -13,8 +14,23 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+
     let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to Postgres.");
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.api_key,
+        timeout,
+    );
 
     let address = format!(
         "{}:{}",
@@ -22,5 +38,5 @@ async fn main() -> std::io::Result<()> {
     );
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
